31bddaa72f992c4bb4642088ff73df1c75ab6f9f
Goutam Pawar <pawargoutam886@gmail.com>
2025-04-25 05:20:52 +0000

    age checker

---
+++ AgeChecker.rb
@@ -0,0 +1,10 @@
+puts "Enter your age:"
+a = gets.chomp.to_i   
+if a >= 18   
+  puts "You are eligible to vote."
+else 
+    puts  "You are not eligible to vote."   
+end  
+
+str = "ssELLO,WORLD!"
+puts str.capitalize	 
